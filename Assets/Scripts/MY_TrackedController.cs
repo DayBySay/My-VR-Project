dc44fc8951823d721133491daf587b3d70f80f41
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-10-12 01:03:20 +0900

    イベントの実装を持つクラスを追加

---
+++ Assets/Scripts/MY_TrackedController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MY_TrackedController : MonoBehaviour {
+	SteamVR_TrackedController trackedController;
+
+	void Start () {
+		trackedController = gameObject.GetComponent<SteamVR_TrackedController> ();
+
+		if (trackedController == null) {
+			trackedController = gameObject.AddComponent<SteamVR_TrackedController> ();
+		}
+
+		trackedController.TriggerClicked += new ClickedEventHandler (DoTriggerClicked);
+		trackedController.TriggerUnclicked += new ClickedEventHandler (DoTriggerUnclicked);
+	}
+
+	public void DoTriggerClicked(object sender, ClickedEventArgs e) {
+		Debug.Log ("DoTriggerClicked");
+	}
+		
+	public void DoTriggerUnclicked(object sender, ClickedEventArgs e) {
+		Debug.Log ("DoTriggerUnclicked");
+
+	}
+
+}
