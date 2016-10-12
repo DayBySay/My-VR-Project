2a5ca68514afda88dba0002b6639eb00eede2f5c
Takayuki_Sei <takayuki_sei@voyagegroup.com>
2016-10-12 13:56:27 +0900

    コントローラとキューブが衝突するところまで

--- Assets/Scripts/MY_TrackedController.cs
+++ Assets/Scripts/MY_TrackedController.cs
@@ -11,25 +11,8 @@ public class MY_TrackedController : MonoBehaviour {
 			trackedController = gameObject.AddComponent<SteamVR_TrackedController> ();
 		}
 
-		trackedController.MenuButtonClicked += new ClickedEventHandler (DoMenuButtonClicked);
-		trackedController.MenuButtonUnclicked += new ClickedEventHandler (DoMenuButtonUnClicked);
 		trackedController.TriggerClicked += new ClickedEventHandler (DoTriggerClicked);
 		trackedController.TriggerUnclicked += new ClickedEventHandler (DoTriggerUnclicked);
-		trackedController.SteamClicked += new ClickedEventHandler (DoSteamClicked);
-		trackedController.PadClicked += new ClickedEventHandler (DoPadClicked);
-		trackedController.PadUnclicked += new ClickedEventHandler (DoPadClicked);
-		trackedController.PadTouched += new ClickedEventHandler (DoPadTouched);
-		trackedController.PadUntouched += new ClickedEventHandler (DoPadTouched);
-		trackedController.Gripped += new ClickedEventHandler (DoGripped);
-		trackedController.Ungripped += new ClickedEventHandler (DoUngripped);
-	}
-
-	public void DoMenuButtonClicked(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoMenuButtonClicked");
-	}
-
-	public void DoMenuButtonUnClicked(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoMenuButtonUnClicked");
 	}
 
 	public void DoTriggerClicked(object sender, ClickedEventArgs e) {
@@ -40,31 +23,7 @@ public class MY_TrackedController : MonoBehaviour {
 		Debug.Log ("DoTriggerUnclicked");
 	}
 
-	public void DoSteamClicked(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoSteamClicked");
-	}
-
-	public void DoPadClicked(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoPadClicked");
-	}
-
-	public void DoPadUnclicked(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoPadUnclicked");
-	}
-
-	public void DoPadTouched(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoPadTouched");
-	}
-
-	public void DoPadUntouched(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoPadUntouched");
-	}
-
-	public void DoGripped(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoGripped");
-	}
-
-	public void DoUngripped(object sender, ClickedEventArgs e) {
-		Debug.Log ("DoUngripped");
+	void OnTriggerEnter(Collider other) {
+		Destroy(other.gameObject);
 	}
 }
